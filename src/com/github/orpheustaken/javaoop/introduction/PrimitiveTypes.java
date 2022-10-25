02272426b30a2867d05bd98a2355cff1341f96df
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-10-25 00:59:22 -0300

    Java Primitive Types

---
+++ src/com/github/orpheustaken/javaoop/introduction/PrimitiveTypes.java
@@ -0,0 +1,16 @@
+package com.github.orpheustaken.javaoop.introduction;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        /*
+            Java primitive types are:
+            int, double, float, char, byte, short, long, boolean
+
+            So, just like C.
+         */
+
+        int age = 20;
+
+        System.out.println("Age is " + age + ", bro.");
+    }
+}
