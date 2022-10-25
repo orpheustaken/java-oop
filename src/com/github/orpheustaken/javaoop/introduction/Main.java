1e91a452eee2b4fb88d1a5195a3af61dbd177374
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-10-25 00:15:13 -0300

    Comments in Java work just like in C

--- src/com/github/orpheustaken/javaoop/introduction/Main.java
+++ src/com/github/orpheustaken/javaoop/introduction/Main.java
@@ -1,6 +1,16 @@
 package com.github.orpheustaken.javaoop.introduction;
 
+/**
+ * Javadoc: Language specific type of comment
+ * that can be used as documentation.
+ * <h3>Also accepts HTML formatting<h3/> <i>What a mess, mon dieu!<i/>
+ */
+
+// Comments in Java work just like in C.
 public class Main {
+    /*
+        Comments in Java work just like in C.
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
