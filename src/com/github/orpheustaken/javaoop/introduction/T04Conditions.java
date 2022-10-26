c31b01859cd541a60c588a5a16cecf986f3a6536
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-10-26 01:26:27 -0300

    Java: Operators

---
+++ src/com/github/orpheustaken/javaoop/introduction/T04Conditions.java
@@ -0,0 +1,15 @@
+package com.github.orpheustaken.javaoop.introduction;
+
+public class T04Conditions {
+    /*
+        Also same as C.
+
+        if (boolean) (exit when true)
+        else if (adds another conditions) (exit when true)
+        else (when if or else if is false)
+
+        Conditional Ternary Operator:
+            var = (condition) ? true : false;
+
+     */
+}
