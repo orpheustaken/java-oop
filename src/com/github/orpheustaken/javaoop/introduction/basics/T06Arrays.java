c8b107ec415d95466ab9727c04a5d681a231bbad
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-11-14 09:02:03 -0300

    Java: Arrays

--- src/com/github/orpheustaken/javaoop/introduction/basics/T06Arrays.java
+++ src/com/github/orpheustaken/javaoop/introduction/basics/T06Arrays.java
@@ -16,8 +16,25 @@ public class T06Arrays {
         numbers[1] = 2;
         numbers[2] = 3;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
+
+        int[] numbers2 = {1, 2, 3, 4, 5};
+
+        // Enhanced for? Foreach.
+        for (int number : numbers2) {
+            System.out.println(number);
+        }
+
+        /*
+        Default initialization values:
+        Objects are initialized as null.
+
+        int, byte, short, double, float, long = 0
+        char = \u0000 (which is unicode for blank value)
+        boolean = false
+        String null (as it is an Object)
+         */
     }
 }
