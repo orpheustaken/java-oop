a8e7c8d545c25a7a301af57906a7fc0dac4048c5
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-11-14 05:52:09 -0300

    Java: Loops

--- src/com/github/orpheustaken/javaoop/introduction/T05LoopStatements.java
+++ src/com/github/orpheustaken/javaoop/introduction/T05LoopStatements.java
@@ -2,5 +2,25 @@ package com.github.orpheustaken.javaoop.introduction;
 
 public class T05LoopStatements {
     public static void main(String[] args) {
+        // Again, Just as C.
+
+        int i = 0;
+
+        // While loop:
+        while (i < 10) {
+            System.out.println("While loop " + i);
+            i++;
+        }
+
+        // Do-while loop:
+        do {
+            System.out.println("Do-while loop " + i);
+            i++;
+        } while (i < 0);
+
+        // For loop:
+        for (int j = 0; j < 10; j++) {
+            System.out.println("For loop: " + j);
+        }
     }
 }
