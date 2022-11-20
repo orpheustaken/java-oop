8f79c8f352554b1dbdc53fd1e937ce9de1cc122d
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-11-20 14:30:44 -0300

    Java: Introduction to Classes

---
+++ src/com/github/orpheustaken/javaoop/introduction/oop/T01Classes/test/StudentTest01.java
@@ -0,0 +1,16 @@
+package com.github.orpheustaken.javaoop.introduction.oop.T01Classes.test;
+
+import com.github.orpheustaken.javaoop.introduction.oop.T01Classes.domain.Student;
+
+public class StudentTest01 {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.age = 21;
+        student.name = "John";
+        student.gender = 'M';
+
+        System.out.println(student.age);
+        System.out.println(student.name);
+        System.out.println(student.gender);
+    }
+}
