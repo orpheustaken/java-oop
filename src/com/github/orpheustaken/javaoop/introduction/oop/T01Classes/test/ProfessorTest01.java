c66af074c14d63708707683df9c2bf42e00ba992
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-11-20 23:40:24 -0300

    Java: Introduction to Methods

--- src/com/github/orpheustaken/javaoop/introduction/oop/T01Classes/test/ProfessorTest01.java
+++ src/com/github/orpheustaken/javaoop/introduction/oop/T01Classes/test/ProfessorTest01.java
@@ -6,12 +6,12 @@ public class ProfessorTest01 {
     public static void main(String[] args) {
         Professor professor = new Professor();
 
-        professor.age = 120;
         professor.name = "Dumbledore";
+        professor.age = 120;
         professor.gender = 'M';
 
-        System.out.println(professor.age);
         System.out.println(professor.name);
+        System.out.println(professor.age);
         System.out.println(professor.gender);
     }
 }
--- src/com/github/orpheustaken/javaoop/introduction/oop/T01Classes/test/StudentTest01.java
+++ src/com/github/orpheustaken/javaoop/introduction/oop/T01Classes/test/StudentTest01.java
@@ -6,12 +6,12 @@ public class StudentTest01 {
     public static void main(String[] args) {
         Student student = new Student();
 
-        student.age = 21;
         student.name = "John";
+        student.age = 21;
         student.gender = 'M';
 
-        System.out.println(student.age);
         System.out.println(student.name);
+        System.out.println(student.age);
         System.out.println(student.gender);
     }
 }
