13da754e884b11e7f01b1d58d121f5fb6f94ad43
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-11-27 21:02:05 -0300

    Java: Instance Initialization Block

---
+++ src/com/github/orpheustaken/javaoop/introduction/oop/T05InitializationBlock/domain/Series.java
@@ -0,0 +1,47 @@
+package com.github.orpheustaken.javaoop.introduction.oop.T05InitializationBlock.domain;
+
+public class Series {
+    private String name;
+    private int[] episodes;
+
+    // Instance Initialization Block.
+    // Gets called before Constructor.
+    // Will always be executed at each Object Initialization.
+
+    // Usually used to initialized attributes.
+    {
+        System.out.print("\n");
+        System.out.println("Inside Instance Initialization Block");
+    }
+
+    // Constructor.
+    public Series(String name) {
+        this.name = name;
+        System.out.println("Constructor 1");
+    }
+
+    // Overloaded Constructor.
+    public Series() {
+        System.out.println("Overloaded Constructor with Iteration");
+        episodes = new int[100];
+
+        for (int i = 0; i < episodes.length; i++) {
+            episodes[i] = i + 1;
+        }
+
+        for (int episode : episodes) {
+            System.out.print(episode + ", ");
+        }
+
+        System.out.print("\n");
+    }
+
+    // Getters.
+    public String getName() {
+        return name;
+    }
+
+    public int[] getEpisodes() {
+        return episodes;
+    }
+}
---
+++ src/com/github/orpheustaken/javaoop/introduction/oop/T05InitializationBlock/test/SeriesTest01.java
@@ -0,0 +1,10 @@
+package com.github.orpheustaken.javaoop.introduction.oop.T05InitializationBlock.test;
+
+import com.github.orpheustaken.javaoop.introduction.oop.T05InitializationBlock.domain.Series;
+
+public class SeriesTest01 {
+    public static void main(String[] args) {
+        Series series = new Series("How I met your mother");
+        Series series1 = new Series();
+    }
+}
